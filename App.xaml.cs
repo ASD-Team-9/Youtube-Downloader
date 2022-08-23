76be88567485f730a03bc303d031d2a13abfda30
Johnny Nguyen <cuongdn2001@gmail.com>
2022-08-23 16:00:44 +1000

    template inital

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace YoutubeDownloader;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
+
---
+++ AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace YoutubeDownloader;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
+
