f42253eee32312ce880cf6ec4fdde68886f1061e
louistsou <louis6251@gmail.com>
2022-09-06 15:09:18 +1000

    Update Program.cs

--- Platforms/iOS/Program.cs
+++ Platforms/iOS/Program.cs
@@ -10,6 +10,7 @@ public class Program
 	{
 		// if you want to use a different Application Delegate class from "AppDelegate"
 		// you can specify it here.
+		int a = 1;
 		UIApplication.Main(args, null, typeof(AppDelegate));
 	}
 }
