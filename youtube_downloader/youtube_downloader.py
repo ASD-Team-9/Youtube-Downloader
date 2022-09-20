7284585e95e25fcd77038be5d0591ec49fb2f938
Zelnonmon <69782394+Zelnonmon045@users.noreply.github.com>
2022-09-20 16:21:27 +1000

    Update youtube_downloader.py

--- youtube_downloader/youtube_downloader.py
+++ youtube_downloader/youtube_downloader.py
@@ -5,16 +5,6 @@ import platform
 if __name__ == "__main__":
     print("Starting")
     path = os.path.abspath(os.path.dirname(__file__))
-
-    if platform.system() == "Darwin":
-        # MAC OS X
-        ytdlp = path + "/resources/yt-dlp_macos"
-    elif platform.system() == "Windows":
-        # Windows (either 32-bit or 64-bit)
-        ytdlp = path + "/resources/yt-dlp"
-
-    subprocess.run([ytdlp,
-                   "https://www.youtube.com/watch?v=T9eTatBQ-ic"])
-    print("Finished Downloading")
-    print("Testing to see if it works")
+    ytdlp = path + ("/resources/yt-dlp_macos" if platform.system() == "Darwin" else "/resources/yt-dlp")
+    subprocess.run([ytdlp, input("YouTube Link: ")])
     input()
