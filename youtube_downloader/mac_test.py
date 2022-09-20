569c3e873bee14425e296e3acf0693ecfa1e4e1d
Lewis123x <79434387+Lewis123x@users.noreply.github.com>
2022-09-20 11:18:45 +1000

    macOs functioning script
    ytdlp mac version was added to resourses, since only the windows version was there.
    mac_test.py is mac copy of the script. Tested and works correctly on my macbook. May have to give permissions for yt-dlp_macos to run.

---
+++ youtube_downloader/mac_test.py
@@ -0,0 +1,9 @@
+import os
+import subprocess
+
+if __name__ == "__main__":
+    print("Starting")
+    path = os.path.abspath(os.path.dirname(__file__))
+    subprocess.run(path + "/resources/yt-dlp_macos https://www.youtube.com/watch?v=T9eTatBQ-ic", shell=True)
+    print("Finished")
+    #input()
