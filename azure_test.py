ab7f31fdc564ae04809a1fa9136f0778795b6cf0
Zelnonmon <69782394+Zelnonmon045@users.noreply.github.com>
2022-10-25 06:58:44 +1100

    Update azure_test.py

--- azure_test.py
+++ azure_test.py
@@ -26,7 +26,8 @@ def pipeline_test():
         url = "https://www.youtube.com/watch?v=_oRxHF8GiUE"
         CONST.FRONTEND.download("Lullaby 「Frozen Starfall」", [CONST.YTDLP, url])
 
-    sleep(1)
+    while CONST.FRONTEND is None:
+        sleep(1)
 
     for each_test in [johnny_test, leo_test, lewis_test, louis_test, ryan_test]:
         status = "Failed"
