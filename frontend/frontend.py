c10e36c4da58525eadb8a5a7e0459643a087553e
sml-234 <101081445+sml-234@users.noreply.github.com>
2022-10-24 12:42:47 +1100

    Update frontend.py

--- frontend/frontend.py
+++ frontend/frontend.py
@@ -164,6 +164,7 @@ class FrontEnd(customtkinter.CTk):
             "Account Page" : Pages.account_page(),
             "New Account Page" : Pages.new_account_page(),
             "History Page" : Pages.history_page(),
+            "Video Player" : Pages.video_player(),
             "Unknown Page" : customtkinter.CTkFrame(
                 corner_radius=0, fg_color=CONST.get_colour("Normal")
             )
